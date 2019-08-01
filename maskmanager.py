32ab8ba432ec106db698d54997d0325a35d25d86
int_0x03 <int_0x03@MacBookPro.local>
2019-08-02 00:34:18 +0300

    Added option for resolve $HEX[...] syntax for human readable print.

--- maskmanager.py
+++ maskmanager.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
-
+import re
+import codecs
 
 #  ? | Charset
 # ===+=========
@@ -25,6 +26,7 @@ MASKS = {
 }
 MASKS['?a'] = MASKS['?l'] + MASKS['?u'] + MASKS['?d'] + MASKS['?s']         # ?a = ?l?u?d?s
 
+VERBOSE_OUTPUT = False
 
 def calculate_difficult(mask):
     result = 1
@@ -43,10 +45,11 @@ def list_from_range(ran):
     return range(start_range, end_range)
 
 def args_parse():
+    global VERBOSE_OUTPUT
     parser = argparse.ArgumentParser(description="Hashcat masks difficulty sorter. Read file from STDIN and print to STDOUT.", \
                                      formatter_class=argparse.RawTextHelpFormatter,                                            \
-                                     epilog='Examples:\n\tmaskmanager -f 1,3-6,9 < input.txt > output.txt\n\t'                  \
-                                                         'maskmanager * > output.txt\n\t'                                       \
+                                     epilog='Examples:\n\tmaskmanager -f 1,3-6,9 < input.txt > output.txt\n\t'                 \
+                                                         'maskmanager * > output.txt\n\t'                                      \
                                                          'maskmanager input.txt input2.txt input3.txt > output.txt')
 
     parser.add_argument('IN_FILES', nargs='*', type=argparse.FileType('r'),                                                    \
@@ -56,53 +59,82 @@ def args_parse():
                                                         "By default fake masks will be removed.",                              \
                                                    action='store_false',                                                       \
                                                    default=True)
+    parser.add_argument("--human-readable", "-hr", help="Resolve $HEX[...] constructions, print as human readable. WARNING: "   \
+                                                        "don't pass this output to hashcat, it is not recognize utf-8",        \
+                                                  action='store_true')
 
     parser.add_argument("--filter-len", "-f", help="Filter masks by length, -f min-max or -f len1,len2", type=str)
     parser.add_argument("--verbose", "-v", help="Verbose output to STDERR", action="store_true")
 
     args = parser.parse_args()
     args_d = vars(args)
-    valid_lenghts = []
+    valid_lengths = []
     if args.filter_len:
         if "," in args.filter_len:
             for length in args.filter_len.split(','):
                 if "-" in length:
-                    valid_lenghts += list_from_range(length)
+                    valid_lengths += list_from_range(length)
                 else:
-                    valid_lenghts.append(int(length))
+                    valid_lengths.append(int(length))
         elif "-" in args.filter_len:
-            valid_lenghts += list_from_range(args.filter_len)
+            valid_lengths += list_from_range(args.filter_len)
         else:
-            valid_lenghts.append(int(args.filter_len))
-    args_d['valid_lenghts'] = valid_lenghts
+            valid_lengths.append(int(args.filter_len))
+    args_d['valid_lengths'] = valid_lengths
+    VERBOSE_OUTPUT = args.verbose
     return args
 def mask_len(mask):
     return len(mask.replace('?', ""))
 
-def main():
-    args = args_parse()
-    lines = []
-    for fd in args.IN_FILES:
-        lines += fd.read().splitlines()
-        fd.close()
+
+def sort_by_difficult(lines, valid_lengths, no_clear):
     result = []
     for line in lines:
         difficulty = calculate_difficult(line)
-        if (not args.no_clear_masks and difficulty == 1) or (args.valid_lenghts and mask_len(line) not in args.valid_lenghts):
+        if (not no_clear and difficulty == 1) or (valid_lengths and mask_len(line) not in valid_lengths):
             continue
         result.append({
-            "key": line,
-            "value": difficulty
+            "mask": line,
+            "difficulty": difficulty
         })
-    result.sort(key=lambda x: x["value"])
-    for masks_dict in result:
-        print(masks_dict['key'])
-    if args.verbose:
+    result.sort(key=lambda x: x["difficulty"])
+    return result
+
+def read_lines(fd_list):
+    lines = []
+    for fd in fd_list:
+        lines += fd.read().splitlines()
+        fd.close()
+    return lines
+
+def resolve_hex_syntax(word):
+    result = re.findall(r"\$HEX\[([0-9a-fA-F]+)\]", word)
+    if result:
+        result = codecs.decode(result[0], "hex").decode('utf-8')
+        if VERBOSE_OUTPUT:
+            eprint('[i] Word "%s" -> "%s"' %(word, result))
+        return result
+    else:
+        return word
+
+def print_out(masks_dicts, human_readable=False):
+    for masks_dict in masks_dicts:
+        if human_readable and all(c in masks_dict['mask'] for c in "$HEX[]"):
+            masks_dict['mask'] = resolve_hex_syntax(masks_dict['mask'])
+        print(masks_dict['mask'])
+    if VERBOSE_OUTPUT:
         total_difficulty = 0
-        for masks_dict in result:
-            eprint("[i] Mask '%s', difficulty: %d combinations." %(masks_dict['key'], masks_dict['value']))
-            total_difficulty += masks_dict['value']
-        eprint("[i] Masks total count: %d\n    Total difficulty: %d combinations" %(len(result), total_difficulty))
+        for masks_dict in masks_dicts:
+            eprint("[i] Mask '%s', difficulty: %d combinations." %(masks_dict['mask'], masks_dict['difficulty']))
+            total_difficulty += masks_dict['difficulty']
+        eprint("[i] Masks total count: %d\n    Total difficulty: %d combinations" %(len(masks_dicts), total_difficulty))
+
+
+def main():
+    args = args_parse()
+    lines = read_lines(args.IN_FILES)
+    lines_dict = sort_by_difficult(lines, args.valid_lengths, args.no_clear_masks)
+    print_out(lines_dict, human_readable=args.human_readable)
 
 
 if __name__ == '__main__':
